7d8218120a8ba31e6cd1059ffb8a6e12ca5bc047
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2023-08-23 09:53:01 +0200

    fix: main page structure
    checks if user is logged in or not and display information accordingly

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,7 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Link from "next/link";
 import LogoutButton from "../components/LogoutButton";
-import SupabaseLogo from "../components/SupabaseLogo";
-import NextJsLogo from "../components/NextJsLogo";
-
+import AddPlayerForm from "./addPlayer/addPlayerForm";
 export const dynamic = "force-dynamic";
 
 export default async function Index() {
@@ -13,7 +11,9 @@ export default async function Index() {
   const {
     data: { user },
   } = await supabase.auth.getUser();
-  return (
+  supabase.from("players").select("*").then(console.log);
+
+  return user?.aud === "authenticated" ? (
     <div className="w-full flex flex-col items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm text-foreground">
@@ -35,10 +35,27 @@ export default async function Index() {
           </div>
         </div>
       </nav>
-
+      <AddPlayerForm uuid={user?.id} />
+    </div>
+  ) : (
+    <div>
+      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        <div className="w-full max-w-4xl flex justify-center items-center p-3 text-sm text-foreground">
+          <Link
+            href="/login"
+            className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+          >
+            Login
+          </Link>
+        </div>
+      </nav>
       <div className="animate-in flex flex-col gap-14 opacity-0 max-w-4xl px-3 py-16 lg:py-24 text-foreground">
         <div className="flex flex-col items-center mb-4 lg:mb-12">
-          <h1 className=" text-white text-4xl">Guess who?</h1>
+          <h1 className=" text-white text-4xl">Welcome to guess who!</h1>
+          <br />
+          <h2 className="text-white text-2xl">
+            Sign up or sign in to get started
+          </h2>
         </div>
       </div>
     </div>
