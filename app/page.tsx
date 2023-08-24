844d81aa8a5eab794433d48fde3089a501032650
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2023-08-24 10:57:28 +0200

    fix: removed unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Link from "next/link";
-import LogoutButton from "../components/LogoutButton";
 import AddPlayerForm from "./addPlayer/addPlayerForm";
-import { PlayersRow } from "./global";
 import { Database } from "@/lib/database.types";
 export const dynamic = "force-dynamic";
 
 export default async function Index() {
   const supabase = createServerComponentClient<Database>({ cookies });
-
   const {
     data: { user },
   } = await supabase.auth.getUser();
@@ -18,18 +15,9 @@ export default async function Index() {
     <div className="w-full flex flex-col items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm text-foreground">
-          {user ? (
-            <div className="w-full flex justify-center items-center gap-4">
-              Hey!
-            </div>
-          ) : (
-            <Link
-              href="/login"
-              className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
-            >
-              Login
-            </Link>
-          )}
+          <div className="w-full flex justify-center items-center gap-4">
+            Hey!
+          </div>
         </div>
       </nav>
       <div className="w-full pt-8 flex justify-center items-center">
