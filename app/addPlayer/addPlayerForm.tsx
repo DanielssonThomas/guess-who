d61fde71028f875d56066ba4cdddbbe68ba05202
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2023-08-24 15:06:40 +0200

    add: photobooth and insert data

--- app/addPlayer/addPlayerForm.tsx
+++ app/addPlayer/addPlayerForm.tsx
@@ -2,29 +2,58 @@ import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { PlayersRow } from "../global";
 import { redirect } from "next/navigation";
+import PhotoBooth from "@/components/PhotoBooth";
+import Questions from "@/components/QuestionList";
+
 const AddPlayerForm = async () => {
   const registerPlayer = async (formData: FormData) => {
     "use server";
     const supabase = createServerActionClient<PlayersRow>({ cookies });
+    const first_name = formData.get("first_name");
+    const last_name = formData.get("last_name");
+    const user_image = formData.get("user_image");
+    const has_glasses = formData.get("has_glasses");
+    const speaks_spanish = formData.get("speaks_spanish");
+    const owns_console = formData.get("owns_console");
+    const likes_sports = formData.get("likes_sports");
+    const has_drivers_licence = formData.get("has_drivers_licence");
+    const uses_mac = formData.get("uses_mac");
+    const plays_instrument = formData.get("plays_instrument");
+    const drinks_coffee = formData.get("drinks_coffee");
+    const watched_GOT = formData.get("watched_GOT");
+    const was_at_way_out_west = formData.get("was_at_way_out_west");
+
     const {
       data: { user },
     } = await supabase.auth.getUser();
+
     const id = user?.id;
-    const first_name = formData.get("first_name");
-    const last_name = formData.get("last_name");
-    const user_image = null;
-    const { error } = await supabase
-      .from("players")
-      .insert({ id, first_name, last_name, user_image });
+    const { error } = await supabase.from("players").insert({
+      id,
+      first_name,
+      last_name,
+      user_image,
+      has_glasses,
+      speaks_spanish,
+      owns_console,
+      likes_sports,
+      has_drivers_licence,
+      uses_mac,
+      plays_instrument,
+      drinks_coffee,
+      watched_GOT,
+      was_at_way_out_west,
+    });
 
     if (error) {
       console.log(error);
     }
+
     redirect("/game");
   };
 
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-2xl justify-center gap-2">
       <form
         className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
         action={registerPlayer}
@@ -43,7 +72,8 @@ const AddPlayerForm = async () => {
           placeholder="Doe"
           required
         />
-
+        <PhotoBooth />
+        <Questions />
         <button className="bg-green-700 rounded px-4 py-2 text-white mb-2">
           Continue
         </button>
--- components/PhotoBooth.tsx
+++ components/PhotoBooth.tsx
@@ -7,17 +7,37 @@ const PhotoBooth = (props: any) => {
   const webcamRef: any = useRef(null);
   const [photoUrl, setPhotoUrl] = useState(null);
 
-  const capture = async () => {
+  const capture = async (e: any) => {
+    e.preventDefault();
     const canvas: any = await html2canvas(webcamRef.current.video);
     const dataUrl: any = canvas.toDataURL("image/png");
     setPhotoUrl(dataUrl);
   };
 
-  return (
-    <div>
+  return photoUrl === null ? (
+    <div className="flex flex-col justify-center">
       <Webcam audio={false} ref={webcamRef} screenshotFormat="image/png" />
-      <button onClick={capture}>Capture Photo</button>
+      <button
+        className="bg-orange-400 rounded px-4 py-2 text-white mb-2"
+        onClick={capture}
+      >
+        Capture photo
+      </button>
       {photoUrl && <img src={photoUrl} alt="Captured" />}
+    </div>
+  ) : (
+    <div className="flex flex-col justify-center">
+      {photoUrl && <img src={photoUrl} alt="Captured" />}
+      <input type="hidden" value={photoUrl} name="user_image" required />
+      <button
+        className="bg-orange-400 rounded px-4 py-2 text-white mb-2"
+        onClick={(e: any) => {
+          setPhotoUrl(null);
+          e.preventDefault();
+        }}
+      >
+        Try again
+      </button>
     </div>
   );
 };
