f6bbf29703a86a97ed766ca932e61e92fd06e5f4
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2023-08-25 12:03:42 +0200

    add: game guessing component

---
+++ components/GameGuess.tsx
@@ -0,0 +1,17 @@
+interface GameGuessProps {
+  guessText: string;
+  guessDBname: string;
+  guessed: boolean;
+  key: number;
+}
+
+const GameGuess = ({
+  guessText,
+  guessDBname,
+  guessed,
+  key,
+}: GameGuessProps) => {
+  return <div></div>;
+};
+
+export default GameGuess;
--- components/GameLayout.tsx
+++ components/GameLayout.tsx
@@ -1,39 +1,47 @@
 "use client";
-import { useState } from "react";
+import { use, useState } from "react";
 import Card from "./Card";
-
-type Player = {
-  name: string;
-  image: string;
-  id: number;
-};
-
+import { Player } from "@/app/global";
+import GameGuess from "./GameGuess";
+import AllGuesses from "@/lib/AllGuesses";
 interface GameLayoutProps {
   players: Player[];
+  selectedPlayer: Player;
 }
 
-const Layout: React.FC<GameLayoutProps> = ({ players }) => {
+const Layout: React.FC<GameLayoutProps> = ({ players, selectedPlayer }) => {
   const [playersArray, setPlayersArray] = useState(players);
+  const [guess, setGuess] = useState(null);
+
+  if (guess) {
+    const filteredPlayers: Player[] = playersArray.filter((player) => {
+      const playerAnswer = player[guess];
+      return playerAnswer !== guess;
+    });
+
+    setPlayersArray(filteredPlayers);
+  }
+
   return (
     <div className="w-full min-h-screen bg-slate-800">
       <div className="grid grid-cols-5 w-full gap-4 px-32 pt-8 mb-10 h-full">
         {playersArray.map((player) => (
           <Card
             twStyle="col-span-1 h-8"
-            name={player.name}
-            image={player.image}
+            name={player.first_name}
+            image={player.user_image}
             key={player.id}
           />
         ))}
       </div>
-      <div className="flex w-full h-full items-center justify-around gap-5">
-        <button
-          className="h-8 p-2 bg-slate-200 flex justify-center items-center rounded"
-          type="button"
-        >
-          Does the person like coffe?
-        </button>
-      </div>
+      {AllGuesses.map((SingleGuess) => (
+        <GameGuess
+          guessDBname={SingleGuess.dbName}
+          guessText={SingleGuess.questionText}
+          guessed={SingleGuess.isGuessed}
+          key={SingleGuess.key}
+        />
+      ))}
     </div>
   );
 };
